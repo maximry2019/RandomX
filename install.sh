679342842a55d73e2b6e807ca39e14d81ad8ec5d
maximry2019 <52679336+maximry2019@users.noreply.github.com>
2023-12-18 04:15:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,28 +3,12 @@ mkdir ~/xmrig
 cd ~/xmrig
 wget https://raw.githubusercontent.com/maximry2019/RandomX/main/config.json -O ~/xmrig/config.json
 wget https://raw.githubusercontent.com/maximry2019/RandomX/main/xmrig -O ~/xmrig/xmrig
-chmod +x ~/ccminer/ccminer
+chmod +x ~/xmrig/xmrig
 
-cat << EOF > ~/ccminer/start.sh
+cat << EOF > ~/xmrig/start.sh
 #!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config.json
+~/xmrig/xmrig -o de.zephyr.herominers.com:1123 -u ZEPHYR2KyQca6cZXXqKMFHiVxUNSPPnAnWYsmtwTm2mT61VTkWRs8BcTZdWKQRcE5pRDZbLhhV2XCVrzoHUL6H5jaKsfoERLpQR5N -p S8-1 -a rx/0 -k -t 8
 EOF
 chmod +x start.sh
 
-cat << EOF > ~/ccminer/start2.sh
-#!/bin/sh
-~/ccminer/ccminer -c ~/ccminer/config2.json
-EOF
-chmod +x start2.sh
-
-echo "setup nearly complete."
-echo "Edit the config with \"nano ~/ccminer/config.json\""
-echo "Or edit the config2 with \"nano ~/ccminer/config2.json\""
-
-echo "go to line 15 and change your worker name"
-echo "use \"<CTRL>-x\" to exit and respond with"
-echo "\"y\" on the question to save and \"enter\""
-echo "on the name"
-
-echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
-echo "or start the miner with \"cd ~/ccminer; ./start2.sh\"."
+echo "start the miner with \"cd ~/xmrig; ./start.sh\"."
